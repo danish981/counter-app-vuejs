de70f2dec3c6c2b873230ba9433a7af7d155e8c3
Danish Mehmood <danishmehmood981@gmail.com>
2023-12-31 22:53:40 +0500

    the very first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+<script setup>
+  import { ref } from 'vue';
+  const count = ref(0);
+</script>
+
+<template>
+  <main class="container">
+    <div class="row text-center justify-content-center mt-5 w-100">
+      <h3>the current counter is ...</h3>
+      <h1 id="counter-element" class="text-center w-100 display-1 font-weight-bold">
+        {{ count }}
+      </h1>
+      <div class="text-center w-100">
+        <button id="minus-button" class="btn btn-warning mx-1" @click="count--">-</button>
+        <button id="reset-button" class="btn btn-warning mx-1" @click="count = 0">reset</button>
+        <button id="plus-button" class="btn btn-warning mx-1" @click="count++">+</button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  font-family: "Jetbrains mono", sans-serif;
+}
+</style>
